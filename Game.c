60479c141e207eada06eb959f9dd6566d4f18632
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2024-01-05 18:20:04 -0500

    cleaning up a little

--- Game.c
+++ Game.c
@@ -13,6 +13,7 @@ int main() {
 while(playing==1){
         printf("you're playing HANGMAN!!! \n");
         int x;
+        int numberOfGuesses=0;
         for(x=0; x< (strlen(word1)); x++){
             printf("_ ");
         }
@@ -20,13 +21,19 @@ while(playing==1){
         char input;    
         x = 0;
         while(x<strlen(word1)){
+            fflush(stdin);
             scanf("%c",&input);
+            fflush(stdin); 
             if(word1[x] == input) {
                 printf("correct\n");
                 x++;	
-            }	
+            }
+            else{
+                printf("wrong\n");
+            }
+            numberOfGuesses++;
         }
-
+        printf("Game complete. You took %d guesses\n", numberOfGuesses);
         printf("continue? (y/n)\n");
         scanf("%c", &input);
         if('n'== input){
