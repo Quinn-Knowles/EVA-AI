c70c4871e1e4967f50ac81f3ac405fb3d6e8855c
Quinn Colette Knowles <Quinn.knowles@maine.edu>
2024-01-05 18:38:14 -0500

    finished with game

--- Game.c
+++ Game.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 //This will be somewhat similar to a game of hangman. It will be a part of early testing for my AI self project.
 //AVA-AI will play this game. 
 //unlike real hangman, there is no loss condition, and letters must be guessed in order.
 int main() {
     int playing = 1;
-   char* word1 = "Cocoa";
-   char* word2 = "Lemon";
-   char* word3 = "Taro";
-   char* guess;
+    char* words[5] = {"Cocoa","Lemon", "Itsuki", "Ranma", "Filia"};
+    char guess[80]="";
+    srand(time(NULL));
 while(playing==1){
         printf("you're playing HANGMAN!!! \n");
+        char* word = words[(rand()%5)];
         int x;
         int numberOfGuesses=0;
-        for(x=0; x< (strlen(word1)); x++){
-            printf("_ ");
+        for(x=0; x< (strlen(word)); x++){
+            strncat(guess, "_ ", 2);
         }
+        printf("%s", guess);
         printf("\n number of characters listed above, please provide input 1 letter at a time \n");
         char input;    
         x = 0;
-        while(x<strlen(word1)){
+        while(x<strlen(word)){
             fflush(stdin);
             scanf("%c",&input);
             fflush(stdin); 
-            if(word1[x] == input) {
+            if(word[x] == input) {
                 printf("correct\n");
-                x++;	
+                guess[(x*2)]=word[x];
+                x++;
+                	
             }
             else{
                 printf("wrong\n");
             }
+            printf("You're still playing HANGMAN.\n Here is where we are at: \n %s \n", guess);
             numberOfGuesses++;
         }
         printf("Game complete. You took %d guesses\n", numberOfGuesses);
